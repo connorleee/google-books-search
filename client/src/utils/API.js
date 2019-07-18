b40551ac33007438a261a7fa8cb11774592d1dce
Connor Lee <45024235+connorleee@users.noreply.github.com>
2019-07-17 23:32:46 -0700

    still trying to figure out react and dotenv. dotenv usually is for back end, but it sounds like you should be able to use it with react.

--- client/src/utils/API.js
+++ client/src/utils/API.js
@@ -3,6 +3,8 @@ import axios from "axios";
 export default {
   // Gets all books
   getBooks: function(search) {
+    console.log(process.env)
+
     return axios.get(`https://www.googleapis.com/books/v1/volumes?key=${process.env.REACT_APP_API_KEY}&q=${search}+intitle`);
   }
 };
